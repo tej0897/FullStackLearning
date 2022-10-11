3cd610f5a0c445a5a50e9bed609e0439eb315e8b
tej0897 <tej0897@gmail.com>
2022-10-11 12:26:50 +0530

    Full Stack Learning - Java8

--- src/Java8/DeclarativeProgramming.java
+++ src/Java8/DeclarativeProgramming.java
@@ -1,13 +1,26 @@
 package Java8;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DeclarativeProgramming {
     public static void main(String[] args) {
         // what style of programming
 
+        // example 1
+
         int sum = IntStream.rangeClosed(0,100)
                 .sum();
         System.out.println("Sum using Declarative method is: " +sum);
+
+        // example 2:
+
+        List<Integer> integerList = Arrays.asList(1,2,3,4,4,5,5,5,6,6,7,8,8,8,9,9,9,0);
+        List<Integer> distinctList = integerList.stream()
+                                        .distinct()
+                                        .toList();
+        System.out.println(distinctList);
     }
 }
--- src/Java8/ImperativeProgramming.java
+++ src/Java8/ImperativeProgramming.java
@@ -1,13 +1,32 @@
 package Java8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ImperativeProgramming {
     public static void main(String[] args) {
         // how style of programming
 
+        // example 1
         int sum = 0;
         for(int i=0; i<=100;i++){
             sum += i;
         }
         System.out.println("Sum using Imperative method is: " +sum);
+
+        // example 2
+
+        List<Integer> integerList = Arrays.asList(1,2,3,4,4,5,5,5,6,6,7,8,8,8,9,9,9,0);
+
+        List<Integer> uniqueList = new ArrayList<>();
+
+        for (Integer i : integerList){
+            if (!uniqueList.contains(i)){
+                uniqueList.add(i);
+            }
+        }
+        System.out.println(uniqueList);
+
     }
 }
